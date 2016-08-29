c1ef15df01b06b53af13f80231931a385af019ab
i709134 <lou.brevetti@voya.com>
2016-08-28 22:20:09 -0400

    fixing sort utility

--- src/utilities/sort.js
+++ src/utilities/sort.js
@@ -35,12 +35,12 @@ export class Sort{
         this.sortType = SORT_TYPE[this.clickCount];
         this.addButtonSort();
         this.event.sortType=this.sortType;
-        this.event.columnName=this.col.name;
+        this.event.columnName=this.col.colLabel;
         this.event.colIndex = this.col.index;
         this.button.dispatchEvent(this.event);
     }
     removeActiveSort(e){
-        if(this.col.name === e.columnName) return;
+        if(this.col.colLabel === e.columnName) return;
         this.button.classList.remove(this.sortType);
         this.clickCount=undefined;
     }
