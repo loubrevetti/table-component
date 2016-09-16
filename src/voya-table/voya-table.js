55ffad71eb335ec2436ea83ecf3f8f844b39564d
i709134 <lou.brevetti@voya.com>
2016-09-16 12:10:07 -0400

    fixing defect

--- src/voya-table/voya-table.js
+++ src/voya-table/voya-table.js
@@ -17,8 +17,7 @@ class VoyaTable extends NativeHTMLElement {
 		this.fetchData();
 	}
 	propertyChangedCallback(prop, oldValue, newValue) {
-		console.log(oldValue);
-		console.log(newValue);
+		if(oldValue === newValue) return;
 		if(prop === "apiUrl") this.fetchData();
 		if(prop === "scrollHeight") this.template.updateTemplateView(this);
 		if((prop=="theme" || prop=="borders" || prop=="rowAlternating" || prop=="sort" || prop=="mobileWidth")){
