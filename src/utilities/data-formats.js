c8b8a1bd5f6a306e4c742f72f79a32c93f09b9ba
i709134 <lou.brevetti@voya.com>
2016-10-04 13:34:56 -0400

    updating css issues and adding new voya-mobile-nav

--- src/utilities/data-formats.js
+++ src/utilities/data-formats.js
@@ -5,10 +5,10 @@ export class Formats{
                 return (isNaN(item) || item === '') ? item : '$ ' + parseFloat(item).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
             },
             pwebCurrency:function(item){
-                return (isNaN(item) || item === '') ? item : '$ ' + parseFloat(item).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+                return (isNaN(item) || item === '') ? item : '$ ' + parseFloat(item).toString().replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
             },
             number:function(item){
-                return (isNaN(item) || item === '') ? item :  parseFloat(item).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+                return (isNaN(item) || item === '') ? item :  parseFloat(item).toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, '$1,');
             },
             percent:function(item){
                 return (isNaN(item) || item === '') ? item : parseFloat(item)+"%";
